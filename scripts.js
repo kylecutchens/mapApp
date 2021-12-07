c83928c3a67f000c56e2e5ab0422fe5f14b74b70
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-07 02:31:14 -0500

    debug

--- scripts.js
+++ scripts.js
@@ -166,7 +166,8 @@ btn.onclick = function () {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "hidden";
+    $('.myModal').attr('display', 'none');
+    console.log('piss')
 }
 
 // When the user clicks anywhere outside of the modal, close it
