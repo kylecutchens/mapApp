ef0b6471d78a3c3511630e6307a7e68e3a892107
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-07 02:29:55 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -166,13 +166,13 @@ btn.onclick = function () {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = "none";
+    modal.style.display = "hidden";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        modal.style.display = "hidden";
     }
 }
 
