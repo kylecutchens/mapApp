778f893bdb61ef34d4a66b2849765220212d456a
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-07 12:03:19 -0500

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -6,7 +6,7 @@ if (isset($_POST['search'])) {
 //Search box value assigning to $bcode variable.
    $bcode = $_POST['search'];
 //Search query.
-   $Query = "SELECT bcode FROM coordinates WHERE bcode LIKE '%$bcode%' LIMIT 5";
+   $Query = "SELECT bcode, bname FROM coordinates WHERE bcode LIKE '%$bcode%' LIMIT 5";
 //Query execution
    $ExecQuery = MySQLi_query($con, $Query);
 //Creating unordered list to display result.
@@ -22,7 +22,7 @@ if (isset($_POST['search'])) {
    <li onclick='fill("<?php echo $Result['bcode']; ?>")'>
    <a>
    <!-- Assigning searched result in "Search box" in "search.php" file. -->
-       <?php echo $Result['bcode']; ?>
+       <?php echo $Result['bcode'] . ' - ' . $Result['bname']; ?>
    </li></a>
    <!-- Below php code is just for closing parenthesis. Don't be confused. -->
    <?php
