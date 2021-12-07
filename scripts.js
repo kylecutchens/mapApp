d9abac60fc3f7f5e78235761ee43187c3a5e2f65
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-07 02:34:20 -0500

    shit

--- scripts.js
+++ scripts.js
@@ -165,10 +165,9 @@ btn.onclick = function () {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    $('.myModal').attr('display', 'none');
-    console.log('piss')
-}
+$('.close').on('click', function() {
+    modal.style.display = "hidden";
+})
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
