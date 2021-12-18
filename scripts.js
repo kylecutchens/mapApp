b118d3915cbe3365193d5a6721c6e259e73a0358
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-18 01:27:25 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -171,7 +171,7 @@ function myMap() {
 
     var mapProp = {
         center: new google.maps.LatLng('32.8275', '-83.6494'),
-        zoom: 16,
+        zoom: 17.5,
         //does the hide thing
        /*styles: [
             {
