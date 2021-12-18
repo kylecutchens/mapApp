399855c55097c967bc5cb3421789b199f1cf7ab3
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-17 23:44:50 -0500

    w

--- scripts.js
+++ scripts.js
@@ -167,11 +167,12 @@ function myMap() {
     var directionsService = new google.maps.DirectionsService();
     var directionsRenderer = new google.maps.DirectionsRenderer();
 
+    //var $style = 
+
     var mapProp = {
         center: new google.maps.LatLng('32.8275', '-83.6494'),
         zoom: 16,
         //does the hide thing
-        mapTypeId: '313c17d8f0620ca9',
         
     };
 
