6814635116e1a152f6eff2f2e40ebbe1c03436ee
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-19 02:17:30 -0500

    Live Search Bar Optimization
    first major refactor. Pretty proud of myself

--- scripts.js
+++ scripts.js
@@ -1,168 +1,5 @@
-//declaring globals because it's 3am and I can't think of a better way to do it
 
-//Getting value from "ajax.php".
-function fill(value) {
-    //Assigning value to "search" div in "search.php" file.
-
-    console.log(value);
-    $(this).find('id').val(value);
-    //Hiding "display" div in "search.php" file.
-    $(this).find('id').hide();
-}
-
-function fill2(Value2) {
-    //Assigning value to "search" div in "search.php" file.
-    $('#search2').val(Value2);
-    //Hiding "display" div in "search.php" file.
-    $('#display2').hide();
-}
-
-
-
-//directions request
-$(document).ready(function () {
-
-
-    $('#confirm').on('click', function () {
-
-        var $search = $('#search').val();
-
-        if (search == "") {
-            $('#display').html("No Location Selected")
-        }
-
-
-        else {
-
-            //$('#message').html($search);
-
-
-            $.ajax({
-
-                type: "POST",
-                url: "getcoords.php",
-                data: { search: $search },
-                success: function (result) {
-
-                    $('#coordinate').attr("value", result);
-                    $("#message").html('Valid Location Confirmed')
-
-                }
-            });
-
-        }
-
-
-    });
-
-    $('#confirm2').on('click', function () {
-
-        var $search2 = $('#search2').val();
-
-        if ($search2 == "") {
-            $('#display2').html("No Location Selected")
-        }
-        else {
-            $.ajax({
-
-                type: "POST",
-                url: "getcoords.php",
-                data: { search: $search2 },
-                success: function (result) {
-
-
-                    $('#coordinate2').attr("value", result);
-                    $("#message2").html('Valid Location Confirmed')
-                }
-            });
-        }
-
-
-    });
-
-
-});
-
-//origin live search bar
-$(document).ready(function () {
-    //On pressing a key on "Search box" in "search.php" file. This function will be called.
-    $("#search").keyup(function () {
-        //Assigning search box value to javascript variable named as "name".
-        var name = $('#search').val();
-        //Validating, if "name" is empty.
-        if (name == "") {
-            //Assigning empty value to "display" div in "search.php" file.
-            $("#display").html("");
-        }
-        //If name is not empty.
-        else {
-            //AJAX is called.
-            $.ajax({
-                //AJAX type is "Post".
-                type: "POST",
-                //Data will be sent to "ajax.php".
-                url: "ajax.php",
-                //Data, that will be sent to "ajax.php".
-                data: {
-                    //Assigning value of "name" into "search" variable.
-                    search: name
-                },
-                //If result found, this funtion will be called.
-                success: function (html) {
-                    //Assigning result to "display" div in "search.php" file.
-                    $("#display").html(html).show();
-                    // $("#display").append("<li value='" + id + "'>" + name + "</li>");
-                }
-            });
-        }
-    });
-});
-//destination live search bar
-$(document).ready(function () {
-    //On pressing a key on "Search box" in "search.php" file. This function will be called.
-    $("#search2").keyup(function () {
-        //Assigning search2 box value to javascript variable named as "name".
-        var name = $('#search2').val();
-        //Validating, if "name" is empty.
-        if (name == "") {
-            //Assigning empty value to "display" div in "search.php" file.
-            $("#display2").html("");
-        }
-        //If name is not empty.
-        else {
-            //AJAX is called.
-            $.ajax({
-                //AJAX type is "Post".
-                type: "POST",
-                //Data will be sent to "ajax.php".
-                url: "ajax.php",
-                //Data, that will be sent to "ajax.php".
-                data: {
-                    //Assigning value of "name" into "search" variable.
-                    search: name
-                },
-                //If result found, this funtion will be called.
-                success: function (html) {
-                    //Assigning result to "display2" div in "search.php" file.
-                    $("#display2").html(html).show();
-                    //$("#display2").append("<li value='" + id + "'>" + name + "</li>");
-                }
-            });
-        }
-    });
-});
-
-
-window.onclick = function (event) {
-    if (event.target.classList.contains('modal')) {
-        for (var index in modals) {
-            if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
-        }
-    }
-}
-
-
-
+//live search bar
 $(document).ready(function () {
 
     $('.textInput').on('keyup', function () {
@@ -201,16 +38,8 @@ $(document).ready(function () {
         }
     });
 
-    $('a').on('click', function () {
-
-        
-
-
-
-
-
     });
-});
+
 
 
 //map callback function. anything relating to the map goes here
@@ -221,7 +50,7 @@ function myMap() {
 
     //var $style = 
 
-    var mapProp = {
+    var $mapProp = {
         center: new google.maps.LatLng('32.8275', '-83.6494'),
         zoom: 18,
         minZoom: 17,
@@ -234,7 +63,7 @@ function myMap() {
 
     };
 
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var map = new google.maps.Map(document.getElementById("map"), $mapProp);
     directionsRenderer.setMap(map);
 
     let markers = {};
