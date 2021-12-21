1491b0e7d4cde0ca78f6a70304bc2fb984b260c2
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-20 22:01:09 -0500

    style

--- index.php
+++ index.php
@@ -130,43 +130,48 @@
 	<div class='buttonHolder' id='buttonHolder'>
 
 		<button id='directions' class='utilityButton'>
-			<img class='buttonIcon' src='images/icons/directions.png'>
 			<div class='infoBox'>
-				
+
 			</div>
+			<img class='buttonIcon' src='images/icons/directions.png'>
+
 		</button>
 
 
 		<button id='filters' class='utilityButton'>
-			<img class='buttonIcon' src='images/icons/filters.png'>
 			<div class='infoBox'>
-				
+
 			</div>
-			
+			<img class='buttonIcon' src='images/icons/filters.png'>
+
+
 		</button>
 
 
 		<button id='accessibility' class='utilityButton'>
-			<img class='buttonIcon' src='images/icons/accessibility.png'>
 			<div class='infoBox'>
-				
+
 			</div>
+			<img class='buttonIcon' src='images/icons/accessibility.png'>
+
 		</button>
 
 
 		<button id='resources' class='utilityButton'>
-			<img class='buttonIcon' src='images/icons/resources.png'>
 			<div class='infoBox'>
-				
+
 			</div>
+			<img class='buttonIcon' src='images/icons/resources.png'>
+
 		</button>
 
 
 		<button id='information' class='utilityButton'>
-			<img class='buttonIcon' src='images/icons/more.png'>
 			<div class='infoBox'>
-				
+
 			</div>
+			<img class='buttonIcon' src='images/icons/more.png'>
+
 		</button>
 
 
