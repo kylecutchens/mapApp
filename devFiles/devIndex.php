a2f2090e702523c64c9c8a7234b4f17a5242b221
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-29 00:35:55 -0500

    style

--- devFiles/devIndex.php
+++ devFiles/devIndex.php
@@ -256,7 +256,7 @@
                 <h3> Edit Marker </h3>
 
                 <div class='preSelect'>
-                    <div class='textHolder'><h4>Select Marker to Begin</h4></div>
+                    <div class='textHolder'><h2>Select Marker to Begin</h2></div>
                     
                 </div>
 
