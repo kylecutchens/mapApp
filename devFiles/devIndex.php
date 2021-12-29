e91f9a40ea46ac634db68b10c89ee94f625eb20a
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-28 21:30:12 -0500

    devtools

---
+++ devFiles/devIndex.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+
+	<title> Mercer University Campus Map </title>
+
+	<header class="header">
+
+
+		<link rel="stylesheet" type="text/css" href="devStyle.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+
+
+		<!-- defunct code for directions search bar. Use only for reference
+
+		<div class="col" style='background-color: lightgrey'>
+			<p id='input'> Please Enter Origin Location </p>
+			  
+			<input type="text" autocomplete="off" id="search" placeholder="Select Origin" />
+			<br>
+			<input type='button' id='confirm' value='Confirm Location'>
+			<br />
+			
+			<div id="display"></div>
+			<div id='message'></div>
+
+
+
+			<div class="col" style='background-color: white'>
+				<p id='input'> Please Enter Destination Location </p>
+
+				<input type="text" autocomplete="off" id="search2" placeholder="Select Destination" />
+				<br>
+				<input type='button' id='confirm2' value='Confirm Location'>
+				<br />
+				
+				<div id="display2"></div>
+				<div id='message2'></div>
+			</div>
+
+	--->
+
+		<div id='header-container' class='header-container'>
+
+			<img class='logo' src='images/misc/logo.png' alt='Mercer University Logo'>
+
+			<input type='text' class='filterBar textInput' id='filterbar' data='filterDisplay' placeholder='Search for buildings here'>
+			<div class='display' id='filterDisplay'></div>
+
+
+		</div>
+
+		<?php
+
+		/*code for connecting to phpmyadmin database. */
+
+		$con = new mysqli('localhost', 'root', 'xXdBLnejWq3h9s', 'mapApp');
+
+		if ($con->connect_error) {
+			die('could not connect to mySQL: ' . $con->connect_error);
+		}
+
+		$centerData = $con->query("SELECT * FROM coordinates WHERE bcode = 'UC'");
+
+		if ($centerData) {
+			$center = $centerData->fetch_assoc();
+		} else
+			die("SELECT CENTER FAILED");
+
+		?>
+
+		</div>
+
+
+		<script> </script>
+	</header>
+
+<body>
+
+	<div id='test'>
+
+	</div>
+
+
+	<?php
+
+	global $con;
+
+	function utf8ize($input)
+	{
+
+		if (is_array($input)) {
+			foreach ($input as $k => $v) {
+				$input[$k] = utf8ize($v);
+			}
+		} else if (is_string($input)) {
+			return utf8_encode($input);
+		}
+		return $input;
+	};
+
+	$markerData = $con->query("SELECT * FROM coordinates ORDER BY indx");
+
+	$r = array();
+	$count = 0;
+
+	while ($row = mysqli_fetch_assoc($markerData)) {
+
+		$r[] = $row;
+		$count = $count + 1;
+	}
+
+	?>
+
+
+	<div style="display: none" id='totalCoords'><?php echo json_encode(utf8ize($r)); ?></div>
+	<input hidden id='coordinate' value='' />
+	<input hidden id='coordinate2' value='' />
+
+
+
+	<div id="map" class='map'></div>
+
+	<div class='buttonHolder' id='buttonHolder'>
+
+		<button id='directions' class='utilityButton' data='directionsDiv'>
+			<div class='infoBox'>
+				<h2>Directions</h2>
+			</div>
+			<img class='buttonIcon' src='images/icons/directions.png' alt='Directions'>
+		</button>
+
+		<button id='filters' class='utilityButton' data='filtersDiv'>
+			<div class='infoBox'>
+				<h2>More Filters</h2>
+			</div>
+			<img class='buttonIcon' src='images/icons/filters.png' alt='More Filters'>
+		</button>
+
+		<button id='accessibility' class='utilityButton' data='accessibilityDiv'>
+			<div class='infoBox'>
+				<h2>Accessibility</h2>
+			</div>
+			<img class='buttonIcon' src='images/icons/accessibility.png' alt='Accessibility Info'>
+		</button>
+
+		<button id='resources' class='utilityButton' data='resourcesDiv'>
+			<div class='infoBox'>
+				<h2>More Resources</h2>
+			</div>
+			<img class='buttonIcon' src='images/icons/resources.png' alt='More Resources'>
+		</button>
+
+		<button id='information' class='utilityButton' data='informationDiv'>
+			<div class='infoBox'>
+				<h2>About</h2>
+			</div>
+			<img class='buttonIcon' src='images/icons/more.png' alt='About'>
+		</button>
+
+	</div>
+
+
+	<div class='utilityDiv-holder'>
+
+		<div class='utilityDiv' id='directionsDiv'>
+
+			<img class='closeButton' data='directionsDiv' src='images/icons/close.png'>
+
+			<h3>Directions</h3>
+
+			<div style='position:absolute'>
+				<input type='text' class='textInput directionsBar' id='originBar' data='originDisplay' placeholder="Enter Starting Point">
+				<div class='display' id='originDisplay'></div>
+			</div>
+
+			<div style='position:absolute; bottom:30px'>
+				<input type='text' class='textInput directionsBar' id='destBar' data='destDisplay' placeholder="Enter Destination">
+				<div class='display' id='destDisplay'></div>
+			</div>
+
+		</div>
+
+		<div class='utilityDiv' id='filtersDiv'>
+
+			<img class='closeButton' data='filtersDiv' src='images/icons/close.png'>
+
+		</div>
+
+		<div class='utilityDiv' id='accessibilityDiv'>
+
+			<img class='closeButton' data='accessibilityDiv' src='images/icons/close.png'>
+
+		</div>
+
+		<div class='utilityDiv' id='resourcesDiv'>
+
+			<img class='closeButton' data='resourcesDiv' src='images/icons/close.png'>
+
+		</div>
+
+		<div class='utilityDiv' id='informationDiv'>
+
+			<img class='closeButton' data='informationDiv' src='images/icons/close.png'>
+
+		</div>
+
+	</div>
+
+
+	<div id='detailsBar' class='detailsBar'>
+
+		<div>
+
+			<div><img class=img-holder id='locationImage' src=''></div>
+			<button class='details-pane-close'>x</button>
+			<div class='details-pane-title'></div>
+
+		</div>
+
+		<div class='details-pane-content'></div>
+	</div>
+
+	<script type="text/javascript" src="devScripts.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA8OXkLqtVF7X51RpqSBL5MjTPZqTNdIo&v=beta&callback=myMap"> </script>
+
+</body>
+
+</html>
---
+++ devFiles/devScripts.js
@@ -0,0 +1,224 @@
+//functions for website utilities
+$(document).ready(function () {
+
+    //clear search bars on refresh
+    $('.textInput').val('');
+
+    //open/close dialogue box when hovering over buttons
+    $(".utilityButton").on('hover', function(){
+        let button = $(this).attr('id')
+        $('#' + button + '> div').addClass('infoBox-visible');   
+    });
+    $(".utilityButton").on('mouseleave', function(){ 
+        let button = $(this).attr('id')
+        $('#' + button + '> div').removeClass('infoBox-visible');
+    });
+
+    //show utility div
+    $(".utilityButton").on('click', function(){
+        $(".utilityDiv").removeClass('utilityDiv-visible');
+        let button = $(this).attr('data');
+        $('#' + button).addClass('utilityDiv-visible');
+    });
+
+    //close button
+    $('.closeButton').on('click', function() {
+        let div = $(this).attr('data')
+        $('#' + div).removeClass('utilityDiv-visible');
+    });
+
+    //live search bar
+    $('.textInput').on('keyup', function () {
+
+        var section = $(this).attr('id');
+        var display = $(this).attr('data');
+
+        let input = $(this).val();
+
+        if (input == "") {
+            $('#' + display).html("");
+        }
+        else {
+            $.ajax({
+                type: 'POST',
+                url: 'ajax.php',
+                data: { search: input },
+                success: function (result) {
+                    $('#' + display).html(result).show();
+
+                    $($('.searchResult')).on('click', function () {
+                        let selection = $(this).attr('id');
+
+                        $('#' + section).attr('value', selection);
+                        $('#' + display).hide();
+                    });
+
+                },
+                error: function () { console.log('live search failed') }
+            });
+        }
+    });
+
+    });
+
+
+
+//map callback function. anything relating to the map goes here
+function myMap() {
+
+    var directionsService = new google.maps.DirectionsService();
+    var directionsRenderer = new google.maps.DirectionsRenderer();
+
+    var $mapProp = {
+        center: new google.maps.LatLng('32.8275', '-83.6494'),
+        zoom: 18,
+        minZoom: 17,
+        maxZoom: 20,
+        mapId: '313c17d8f0620ca9',
+        disableDoubleClickZoom: true,
+        disableDefaultUI: true,
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), $mapProp);
+    directionsRenderer.setMap(map);
+
+    let markers = {};
+    let infowindows = {};
+
+
+
+    const coordsArr = JSON.parse($('#totalCoords').text());
+
+    for (let i = 0; i < coordsArr.length; i++) {
+
+        const coord = coordsArr[i];
+        markers[coord.bcode] = new google.maps.Marker({
+            position: new google.maps.LatLng(coord.lat, coord.lon),
+            title: coord.bcode
+        });
+
+        infowindows[coord.bcode] = new google.maps.InfoWindow({ content: coord.bcode + " - " + coord.bname });
+
+        markers[coord.bcode].setMap(map);
+        markers[coord.bcode].addListener("mouseover", () => { infowindows[coord.bcode].open({ anchor: markers[coord.bcode], map }) });
+        markers[coord.bcode].addListener("mouseout", () => { infowindows[coord.bcode].close() });
+        markers[coord.bcode].addListener("click", () => { showInfoDiv(coord.indx) });
+    }
+
+    $("#getDirections").on('click', function () {
+
+        var $origin = $('#coordinate').val();
+        var $destination = $('#coordinate2').val();
+
+        var $request = {
+
+            origin: $origin,
+            destination: $destination,
+            travelMode: 'WALKING'
+
+        };
+
+        directionsService.route($request, function (result, status) {
+
+            if (status == 'OK') {
+
+                $('.modal').attr('display', 'none');
+                clearMarkers();
+                directionsRenderer.setDirections(result);
+                $('#myModal1').attr('display', 'none');
+                $('.clearDirections').addClass('clearDirections-show');
+            }
+
+            $('.clearDirections').on('click', function () {
+
+                directionsRenderer.set('directions', null);
+                $('.clearDirections').removeClass('clearDirections-show');
+                addMarkers();
+
+            });
+
+
+
+        });
+    });
+
+    $('#filterbar').on('change', function () {
+
+        var input = $('#filterbar').val()
+
+
+        if (input == "") {
+            addMarkers();
+        }
+        else {
+            clearMarkers(input);
+
+            $.ajax({
+
+                type: "POST",
+                url: "filter.php",
+                data: { input: input },
+                success: function (result) {
+
+                    for (let i = 0; i < result.length; i++) {
+
+                        markers[result[i]].setMap(map);
+
+                    }
+
+                },
+                dataType: "json",
+                error: function () { console.log('filter failed') }
+            });
+        }
+
+
+
+    });
+
+    $(".details-pane-close").on('click', function () {
+        $('.detailsBar').removeClass('details-pane-visible');
+    })
+
+    function clearMarkers() {
+        for (let i = 0; i < coordsArr.length; i++) {
+
+            const coord = coordsArr[i];
+            markers[coord.bcode].setMap(null);
+
+        }
+
+    }
+
+    function addMarkers() {
+        for (let i = 0; i < coordsArr.length; i++) {
+
+            const coord = coordsArr[i];
+            markers[coord.bcode].setMap(map);
+
+        }
+    }
+
+    function showInfoDiv(indx) {
+
+        let markerData = coordsArr[indx]
+        const $detailsDiv = $('.detailsBar')
+        const $titleDiv = $('.details-pane-title');
+        const $contentDiv = $('.details-pane-content');
+        const $imageDiv = $('#locationImage');
+
+
+        $detailsDiv.removeClass('details-pane-visible');
+
+        $imageDiv.attr('src', markerData.image);
+        $titleDiv.text(markerData.bname);
+        $contentDiv.text(markerData.description);
+
+        $detailsDiv.addClass('details-pane-visible');
+
+        $detailsDiv.removeClass('details-pane--visible');
+
+
+
+    }
+};
