e30180208d9bb1cb92ccda06db1e2051723ae835
Kyle Cutchens <kylecutchens@gmail.com>
2021-12-29 20:42:44 -0500

    cleanup

--- devFiles/devIndex.php
+++ devFiles/devIndex.php
@@ -42,16 +42,7 @@
 
 	--->
 
-		<div id='header-container' class='header-container'>
-
-			<img class='logo' src='images/misc/logo.png' alt='Mercer University Logo'>
-
-			<input type='text' class='filterBar textInput' id='filterbar' data='filterDisplay' placeholder='Search for buildings here'>
-			<div class='display' id='filterDisplay'></div>
-
-
-		</div>
-
+		
 		<?php
 
 		/*code for connecting to phpmyadmin database. */
@@ -71,17 +62,6 @@
 
 		?>
 
-		</div>
-
-
-		<script> </script>
-	</header>
-
-<body>
-
-	<div id='test'>
-
-	</div>
 
 
 	<?php
@@ -116,114 +96,8 @@
 
 
 	<div style="display: none" id='totalCoords'><?php echo json_encode(utf8ize($r)); ?></div>
-	<input hidden id='coordinate' value='' />
-	<input hidden id='coordinate2' value='' />
-
-
-
-
-
+	
 	<div id="map" class='map'></div>
-
-	<div class='buttonHolder' id='buttonHolder'>
-
-		<button id='directions' class='utilityButton' data='directionsDiv'>
-			<div class='infoBox'>
-				<h2>Directions</h2>
-			</div>
-			<img class='buttonIcon' src='images/icons/directions.png' alt='Directions'>
-		</button>
-
-		<button id='filters' class='utilityButton' data='filtersDiv'>
-			<div class='infoBox'>
-				<h2>More Filters</h2>
-			</div>
-			<img class='buttonIcon' src='images/icons/filters.png' alt='More Filters'>
-		</button>
-
-		<button id='accessibility' class='utilityButton' data='accessibilityDiv'>
-			<div class='infoBox'>
-				<h2>Accessibility</h2>
-			</div>
-			<img class='buttonIcon' src='images/icons/accessibility.png' alt='Accessibility Info'>
-		</button>
-
-		<button id='resources' class='utilityButton' data='resourcesDiv'>
-			<div class='infoBox'>
-				<h2>More Resources</h2>
-			</div>
-			<img class='buttonIcon' src='images/icons/resources.png' alt='More Resources'>
-		</button>
-
-		<button id='information' class='utilityButton' data='informationDiv'>
-			<div class='infoBox'>
-				<h2>About</h2>
-			</div>
-			<img class='buttonIcon' src='images/icons/more.png' alt='About'>
-		</button>
-
-	</div>
-
-
-	<div class='utilityDiv-holder'>
-
-		<div class='utilityDiv' id='directionsDiv'>
-
-			<img class='closeButton' data='directionsDiv' src='images/icons/close.png'>
-
-			<h3>Directions</h3>
-
-			<div style='position:absolute'>
-				<input type='text' class='textInput directionsBar' id='originBar' data='originDisplay' placeholder="Enter Starting Point">
-				<div class='display' id='originDisplay'></div>
-			</div>
-
-			<div style='position:absolute; bottom:30px'>
-				<input type='text' class='textInput directionsBar' id='destBar' data='destDisplay' placeholder="Enter Destination">
-				<div class='display' id='destDisplay'></div>
-			</div>
-
-		</div>
-
-		<div class='utilityDiv' id='filtersDiv'>
-
-			<img class='closeButton' data='filtersDiv' src='images/icons/close.png'>
-
-		</div>
-
-		<div class='utilityDiv' id='accessibilityDiv'>
-
-			<img class='closeButton' data='accessibilityDiv' src='images/icons/close.png'>
-
-		</div>
-
-		<div class='utilityDiv' id='resourcesDiv'>
-
-			<img class='closeButton' data='resourcesDiv' src='images/icons/close.png'>
-
-		</div>
-
-		<div class='utilityDiv' id='informationDiv'>
-
-			<img class='closeButton' data='informationDiv' src='images/icons/close.png'>
-
-		</div>
-
-	</div>
-
-
-	<div id='detailsBar' class='detailsBar'>
-
-		<div>
-
-			<div><img class=img-holder id='locationImage' src=''></div>
-			<button class='details-pane-close'>x</button>
-			<div class='details-pane-title'></div>
-
-		</div>
-
-		<div class='details-pane-content'></div>
-	</div>
 
     <div id='toolsBar' class='detailsBar details-pane-visible' style='width: 35vh; opacity: 1; background-color:#333'>
 
@@ -260,10 +134,6 @@
 
         </div>
         
-        
-
-
-
     </div>
 
 
