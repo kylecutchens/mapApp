5c9ab558d800fa3aad8bbddc1b60873d004a2e78
Kyle Cutchens <kylecutchens@gmail.com>
2022-01-01 20:32:58 -0500

    devtool

--- devFiles/devAjax.php
+++ devFiles/devAjax.php
@@ -14,10 +14,10 @@
         $lon = $_POST['lon'];
 
     //get the max index, convert to usable data, add 1
-        $maxIndx = $con -> query('SELECT MAX(indx) FROM coordinatesTest');
+        $indx = $con -> query('SELECT MAX(indx) FROM coordinatesTest');
         $indx = $indx -> fetch_array();
         $maxIndx = intval($indx[0]);
-        $maxIndx = $maxIndx = 1;
+        $maxIndx = $maxIndx + 1;
 
 
         $Result = 'Query Failed';
@@ -28,8 +28,7 @@
             case 'insert':
                 
                 $selectQuery = $con -> query(
-                    "INSERT INTO coordinatesTest (bname, bcode, description, image, lat, lon, indx)
-                     VALUES ($name, $code, $description, $image, $lat, $lon, $maxIndx)");
+                    "INSERT INTO `coordinatesTest` (`bname`, `bcode`, `description`, `image`, `lat`, `lon`, `indx`) VALUES ('$name', '$code', '$description', '$image', '$lat', '$lon', '$maxIndx');"); 
                 if($selectQuery){
                    echo "New Record Created Successfully";
                 }
@@ -56,11 +55,11 @@
                 break;
 */        
 
-      //  }  
+        } 
 
-        echo 'Information Received: ' . $name . ', ' . $code . ', ' . $description . ', ' . $image . ', ' . $lat . ', ' . $lon;
+        echo 'Information Received: ' . $name . ', ' . $code . ', ' . $description . ', ' . $image . ', ' . $lat . ', ' . $lon . ', ' . $maxIndx;
         
-    }
+            //}
   //  else
   //  {
  //      echo 'Query Failed: ' .  mysqli_error($con);
